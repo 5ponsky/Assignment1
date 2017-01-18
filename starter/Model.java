04669890e3341f5167a0a969bbda7b9c089ba739
5ponsky <mailgd9@gmail.com>
2017-01-18 07:09:35 -0600

    Init

---
+++ starter/Model.java
@@ -0,0 +1,25 @@
+class Model {
+  int turtle_x, turtle_y;
+  int dest_x, dest_y;
+
+  Model() {
+
+  }
+
+  public void update() {
+    if(this.turtle_x < this.dest_x)
+      this.turtle_x += Math.min(4, dest_x - turtle_x);
+    else if(this.turtle_x > this.dest_x)
+      this.turtle_x -= Math.min(4, dest_x - turtle_x);
+
+    if(this.turtle_y < this.dest_y)
+      this.turtle_y += Math.min(4, dest_x - turtle_x);
+    else if(this.turtle_y > this.dest_y)
+      this.turtle_y -= Math.min(4, dest_x - turtle_x);
+  }
+
+  public void setDestination(int x, int y) {
+    this.dest_x = x;
+    this.dest_y = y;
+  }
+}
